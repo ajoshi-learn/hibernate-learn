b76ef346c55f2ca6ab3b6d2ccb279622ece0ec24
Artur <arturjoshi.mailbox@gmail.com>
2016-09-17 21:08:21 +0300

    removed target folder

---
+++ src/main/java/app/chapter2/enumsexample/Phone.java
@@ -0,0 +1,19 @@
+package app.chapter2.enumsexample;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Phone {
+    @Id
+    private Long id;
+    @Column(name = "phone_number")
+    private String number;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "phone_type")
+    private PhoneType phoneType;
+}
---
+++ src/main/java/app/chapter2/enumsexample/PhoneType.java
@@ -0,0 +1,6 @@
+package app.chapter2.enumsexample;
+
+public enum PhoneType {
+    LAND_LINE,
+    MOBILE
+}
